1ace89da8369ef12fb7b55cacb3d2cb85cb44c4e
NPT0116 <thanh1612004@gmail.com>
2025-01-13 22:14:48 +0700

    add global exception handler, fluent validation

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,37 @@
+using backend.src.Data;
+using backend.src.Exceptions.Example;
+using backend.src.Middlewares;
+using backend.src.Validators.User;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Scalar.AspNetCore;
+using Serilog;
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+// serilog
 
+
+// global exception handler
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+    builder.Host.UseSerilog((context, loggerConfiguration) =>
+    {
+        loggerConfiguration.WriteTo.Console();
+        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+    });
+builder.Services.AddValidatorsFromAssemblyContaining<UserRegistorRequestValidator>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,31 +46,13 @@ if (app.Environment.IsDevelopment())
 
     });
 }
+app.MapGet("/", () => { throw new ProductNotFoundException(Guid.NewGuid()); });app.UseHttpsRedirection();
+app.UseExceptionHandler();
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+
+
+
