ab95676c32b4ba2ddbb6fe5400c63a72d98b194d
NPT0116 <thanh1612004@gmail.com>
2025-01-13 23:43:03 +0700

    feat: nothing

--- src/Program.cs
+++ src/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
